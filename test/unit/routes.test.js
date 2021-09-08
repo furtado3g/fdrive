69f6fefd4c9b0b7ce91f47acb2ba74b55515e0fa
Lucas Antunes Almeida Furtado <lucas_shiguioka@hotmail.com>
2021-09-08 15:46:33 -0300

    finishes routes test

--- test/unit/routes.test.js
+++ test/unit/routes.test.js
@@ -28,18 +28,54 @@ describe("Routes Test Suite", () => {
       values: () => Object.values(defaultParams),
     };
 
-    test("given a inexistent route it should choose default route", () => {
+    test("given a inexistent route it should choose default route async", () => {
       const routes = new Routes();
       const params = {
         ...defaultParams,
       };
       params.request.method = "inexistent";
-      routes.handler(...params.values());
+      await routes.handler(...params.values());
       expect(params.response.end).toHaveBeenCalledWith("hello world");
     });
-    test.todo("it should set any request with CORS enabled");
-    test.todo("give method OPTIONS it should chose option route");
-    test.todo("give method GET it should chose get route");
-    test.todo("give method POST it should chose post route");
+    test("it should set any request with CORS enabled", async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+      params.request.method = "inexistent";
+      await routes.handler(...params.values());
+      expect(params.response.setHeader).toHaveBeenCalledWith(
+        "Access-Control-Allow-origin",
+        "*"
+      );
+    });
+    test("give method OPTIONS it should chose option route", async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+      params.request.method = "OPTIONS";
+      await routes.handler(...params.values());
+      expect(params.response.writeHead).toHaveBeenCalledWith(204);
+      expect(params.response.end).toHaveBeenCalledWith("Ola mundo");
+    });
+    test("give method GET it should chose get route", async () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+      params.request.method = "GET";
+      await routes.handler(...params.values());
+      expect(params.response.end).toHaveBeenCalledWith("hola mundo");
+    });
+    test("give method POST it should chose post async route", () => {
+      const routes = new Routes();
+      const params = {
+        ...defaultParams,
+      };
+      params.request.method = "POST";
+      await routes.handler(...params.values());
+      expect(params.response.end).toHaveBeenCalledWith("hello world");
+    });
   });
 });
